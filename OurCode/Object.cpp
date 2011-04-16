39b2cc669fed500bfe9f90b8952393f228faa9f0
ihatton <ihatton@siue.edu>
2011-04-16 16:44:06 -0500

    fixed the compile error in object.cpp

--- OurCode/Object.cpp
+++ OurCode/Object.cpp
@@ -1,62 +1,59 @@
 #include <iostream>
 #include "Object.h"
 
+using namespace std;
 
-	
+	//Default Constructor
 	Object::Object()
 	{
+		speed = 0;
+		maxSpeed = 0;
+		xcoord = 0;
+		ycoord = 0;
+		direction = 0;
+		height = 0;
+		width = 0;
+		alive = 0;
+		isKillable = 0;
 	}
 
-	Object::Object(int dSpeed, double dDirection, double dX, double dY, bool dKillable, int dmaxSpeed, int dHeight, int dWeigth, bool dAlive)
+	//Initializing Constructor
+	Object::Object(int dSpeed, double dDirection, double dX, double dY, 
+		bool dKillable, int dmaxSpeed, int dHeight, int dWeigth, bool dAlive)
 	{
 		speed = dSpeed;
-		direction = dDirection;
+		maxSpeed = dmaxSpeed;
 		xcoord = dX;
 		ycoord = dY;
-		isKillable = dKillable;
-		maxSpeed = dmaxSpeed;
+		direction = dDirection;
 		height = dHeight;
 		width = dWeigth;
 		alive = dAlive;
-
+		isKillable = dKillable;
 	}
 
+	//Object destructor
+	Object::~Object()
+	{
+		Die();
+	}
 
+	//Returns the speed of the Object
+	int Object::getSpeed()
+	{
+		return speed;
+	}
 
 	//Sets the speed of the Object
 	void Object::setSpeed(int newSpeed)
 	{
 		speed = newSpeed;
 	}
-	//Returns the speed of the Object
-	int Object::getSpeed()
-	{
-		return speed;
-	}
-	//Sets the direction of the Object
-	void Object::setDirection(double newDirection)
-	{
-		direction = newDirection;
-	}
-	//Gets the Direction of the Object
-	double Object::getDirection()
-	{
-		return direction;
-	}
-	void Object::Move()
-	{
 
-	}
-	void Object::Shoot()
+	//Gets the X coordinate of the object
+	double Object::getX()
 	{
-		
-	}
-	void Object::Die()
-	{
-		if(isKillable == true)
-		{
-		delete this;
-		}
+		return xcoord;
 	}
 
 	//Sets the X coordinate for the location of the object.
@@ -64,23 +61,45 @@
 	{
 		xcoord = newX;
 	}
-	//Returns the X coordinate of the object
-	double Object::getX()
-	{
-		return xcoord;
-	}
-	//Sets the Y coordinate for the location of the object
-	void Object::setY(double newY)
-	{
-		ycoord = newY;
-	}
-	//Returns the X coordinate of the object.
+
+	//Gets the Y coordinate of the object.
 	double Object::getY()
 	{
 		return ycoord;
 	}
 
-	Object::~Object()
+	//Sets the Y coordinate for the location of the object
+	void Object::setY(double newY)
 	{
-		Die();
+		ycoord = newY;
+	}
+
+	//Gets the direction of the Object
+	double Object::getDirection()
+	{
+		return direction;
+	}
+
+	//Sets the direction of the Object
+	void Object::setDirection(double newDirection)
+	{
+		direction = newDirection;
+	}
+
+	void Object::Move()
+	{
+
+	}
+
+	void Object::Shoot()
+	{
+		
+	}
+
+	void Object::Die()
+	{
+		if(isKillable == true)
+		{
+			delete this;
+		}
 	}
--- OurCode/Object.h
+++ OurCode/Object.h
@@ -4,41 +4,36 @@
 #include <iostream>
 using namespace std;
 
-
-
-class Object {
-
+class Object 
+{
 public:
 	// List of class variables
 	int speed;
-	double direction;
+	int maxSpeed;
 	double xcoord;
 	double ycoord;
-	bool isKillable;
-	int maxSpeed;
+	double direction;
 	int height;
 	int width;
 	bool alive;
-
+	bool isKillable;
+		
 	// List of Methods
 	Object();
-	Object(int dSpeed, double dDirection, double dX, double dY, bool dKillable, int dmaxSpeed, int dHeight, int dWeigth, bool dAlive);
-	void setSpeed(int newSpeed);
+	Object(int dSpeed, double dDirection, double dX, double dY, bool dKillable, 
+		int dmaxSpeed, int dHeight, int dWeigth, bool dAlive);
+	~Object();
+
 	int getSpeed();
-	void setDirection(double newDirection);
+	void setSpeed(int newSpeed);
+	double getX();
+	void setX(double newX);
+	double getY();
+	void setY(double newY);
 	double getDirection();
+	void setDirection(double newDirection);
 	void Move();
 	void Shoot();
 	void Die();
-	void setX(double newX);
-	double getX();
-	void setY(double newY);
-	double getY();
-	~Object();
-
-	
-
-
-}
-
+};
 #endif
