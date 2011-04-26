0be3349bbe51012fb53e05eac1825de7db457922
ihatton <ihatton@siue.edu>
2011-04-25 20:42:09 -0500

    clean-up has begun

--- GraphicsCode/cs325graphicsdriver.cpp
+++ GraphicsCode/cs325graphicsdriver.cpp
@@ -24,7 +24,11 @@ int main(int argc, char* argv[])
 	Point2D p3(CS325Graphics::X_MIN, CS325Graphics::Y_MIN);
 	Point2D p4(CS325Graphics::X_MAX, CS325Graphics::Y_MAX);
 
-	//Points 41, 42, 45, 46 control the sandbox. DON"T MESS WITH THEM!
+	//Area coordinates
+	Point3D p41( 50.8,-2.8,  50.8);
+	Point3D p42(-50.8,-2.8,  50.8);
+	Point3D p45( 50.8,-2.8, -50.8);
+	Point3D p46(-50.8,-2.8, -50.8);
 
 	Point3D p30(0.5,  0.5,-3.5);
 	Point3D p31(0.5, -0.5,-3.5);
@@ -35,9 +39,8 @@ int main(int argc, char* argv[])
 	Point3D p36(-0.5,-0.5,-1.5);
 	Point3D p37(-0.5, 0.5,-1.5);
 
-	Point3D p40( -70.8, 28.8, -50.8);
-	Point3D p41( 50.8,-2.8,  50.8);
-	Point3D p42(-50.8,-2.8,  50.8);
+	Point3D p40(-70.8, 28.8, -50.8);
+	
 	Point3D p43(-58.8, 25.8,  50.8);
 	Point3D p44( 50.8, 50.8, -50.8);
 	Point3D p45( 50.8,-2.8, -50.8);
@@ -64,8 +67,6 @@ int main(int argc, char* argv[])
 	Point3D p66(2.5,-0.5,  16.5);
 	Point3D p67(2.5, 0.5,  16.5);
 
-	
-
 	Point2D viewPos;
 	Vector2D viewDir;
 	Vector3D deltaV;
@@ -73,11 +74,8 @@ int main(int argc, char* argv[])
 
 
 	// move view position
-
-	for(int i = 0; i < MOVE_TEST; i){
-		/*window.DrawLineOnScreen(p1, p2);*/
-		//window.DrawLineOnScreen(p4, p3);
-
+	for(int i = 0; i < MOVE_TEST; i)
+	{
 		window.DrawLineInSpace(p30, p31);
 		window.DrawLineInSpace(p31, p32);
 		window.DrawLineInSpace(p32, p33);
@@ -117,15 +115,9 @@ int main(int argc, char* argv[])
 		window.DrawLineInSpace(p62, p66);
 		window.DrawLineInSpace(p63, p67);
 
-
-		//window.DrawLineInSpace(p40, p41);
 		window.DrawLineInSpace(p41, p42);
 		window.DrawLineInSpace(p42, p43);
-		//window.DrawLineInSpace(p43, p40);
-		//window.DrawLineInSpace(p44, p45);
 		window.DrawLineInSpace(p45, p46);
-		//window.DrawLineInSpace(p46, p47);
-		//window.DrawLineInSpace(p47, p44);
 		window.DrawLineInSpace(p40, p45);
 		window.DrawLineInSpace(p41, p45);
 		window.DrawLineInSpace(p42, p46);
@@ -137,64 +129,64 @@ int main(int argc, char* argv[])
 		window.DrawLineInSpace(p45, p48);
 
 
-				if(GetAsyncKeyState(VK_DOWN)) // the DOWN arrow was pressed, let's do something
+		// the DOWN arrow was pressed, let's do something
+		if(GetAsyncKeyState(VK_DOWN)) 
 		{
-		delta = -.1;
-		viewPos.setY(viewPos.getY() + cos(-viewDir.getAngle())*delta);
-		viewPos.setX(viewPos.getX() + sin(-viewDir.getAngle())*delta);
-		window.SetViewPosition(viewPos);
-		cout << "view pos: " << viewPos.toString() << endl;
+			delta = -.1;
+			viewPos.setY(viewPos.getY() + cos(-viewDir.getAngle())*delta);
+			viewPos.setX(viewPos.getX() + sin(-viewDir.getAngle())*delta);
+			window.SetViewPosition(viewPos);
+			cout << "view pos: " << viewPos.toString() << endl;
 
-		window.DisplayNow();
-		Sleep(50);
-				}
+			window.DisplayNow();
+			Sleep(50);
+		}
 
-			if(GetAsyncKeyState(VK_UP)) // the UP arrow was pressed, let's do something
+		// the UP arrow was pressed, let's do something
+		if(GetAsyncKeyState(VK_UP)) 
 		{
-		delta = .1;
-		viewPos.setY(viewPos.getY() + cos(-viewDir.getAngle())*delta);
-		viewPos.setX(viewPos.getX() + sin(-viewDir.getAngle())*delta);
-		window.SetViewPosition(viewPos);
-		cout << "view pos: " << viewPos.toString() << endl;
+			delta = .1;
+			viewPos.setY(viewPos.getY() + cos(-viewDir.getAngle())*delta);
+			viewPos.setX(viewPos.getX() + sin(-viewDir.getAngle())*delta);
+			window.SetViewPosition(viewPos);
+			cout << "view pos: " << viewPos.toString() << endl;
 
-		window.DisplayNow();
-		Sleep(50);
-				}
-				if(GetAsyncKeyState(VK_RIGHT)) // the RIGHT arrow was pressed, let's do something
+			window.DisplayNow();
+			Sleep(50);
+		}
+				
+		 // the RIGHT arrow was pressed, let's do something
+		if(GetAsyncKeyState(VK_RIGHT))
 		{
-	
-		delta = .1;
+			delta = .1;
 
-		viewDir.setAngle(viewDir.getAngle()+delta);
-		window.SetViewDirection(viewDir);
-		cout << "view dir: " << viewDir.getAngle() << endl;
+			viewDir.setAngle(viewDir.getAngle()+delta);
+			window.SetViewDirection(viewDir);
+			cout << "view dir: " << viewDir.getAngle() << endl;
 
-		window.DisplayNow();
-		Sleep(50);	
-			}
+			window.DisplayNow();
+			Sleep(50);	
+		}
 			
-			if(GetAsyncKeyState(VK_LEFT)) // the LEFT arrow was pressed, let's do something
+		// the LEFT arrow was pressed, let's do something	
+		if(GetAsyncKeyState(VK_LEFT)) 
 		{
-	
-		delta = -.1;
+			delta = -.1;
 
-		viewDir.setAngle(viewDir.getAngle()+delta);
-		window.SetViewDirection(viewDir);
-		cout << "view dir: " << viewDir.getAngle() << endl;
+			viewDir.setAngle(viewDir.getAngle()+delta);
+			window.SetViewDirection(viewDir);
+			cout << "view dir: " << viewDir.getAngle() << endl;
 
-		window.DisplayNow();
-		Sleep(50);	
-			}
+			window.DisplayNow();
+			Sleep(50);	
+		}
 
+		// escape key exits the game
 		if(GetAsyncKeyState(VK_ESCAPE))
 		{
 			return 1;
 		}
-	}
-
-
-		
-	
+	}	
 }
 
 
